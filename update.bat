9ff2bd3b12c7e4f614b2c387c8a67886c172d83c
lucaslu <51876384+StockFi@users.noreply.github.com>
2021-12-15 21:31:08 +0800

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,9 +1,1 @@
-tasklist /FI "IMAGENAME eq monero-wallet-gui.exe" /FO CSV > search.log
-FOR /F %%A IN (search.log) do if %%A == 資訊: goto process_off
-:process_on
-goto end
-:process_off
-start "C:\Program Files\Monero GUI Wallet\monero-wallet-gui.exe"
-:end
-
-del- search.log
+:: 
